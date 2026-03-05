1fa4ed1230bc0fe4a6b30d7d50be20137504997f
Leo Dev <leoneljovel49@gmail.com>
2026-03-06 00:25:23 +0100

    Adding the Project Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/form';
+import TextList from './components/textList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className='to-do-list'>
+        <h1>TODOS</h1>
+        <div className='container'>
+          <Form></Form>
+        </div>
+
+        <div className='text-todo-container'>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        <TextList text={"Learn React"}></TextList>
+        </div>
+      </main>
     </div>
   );
 }
